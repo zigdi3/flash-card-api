426852401fec170d44b601b2107ea8b0faa8704e
Diego Araujo <diegozigoto@gmail.com>
2024-12-14 23:31:49 -0300

    wip: test node version 20.x.x

--- src/modules/flash-card/flash-card.repository.ts
+++ src/modules/flash-card/flash-card.repository.ts
@@ -1,48 +1,45 @@
 import { Injectable } from '@nestjs/common';
 import { FlashCard } from '@prisma/client';
 import { PrismaService } from '../infra';
-import { ObjectId } from 'bson';
 
 @Injectable()
 export class FlashCardRepository {
-  constructor(private repo: PrismaService) { }
+  constructor(private repo: PrismaService) {}
 
   async findById(id: string): Promise<FlashCard[]> {
     const result = [];
-    result.push(await this.repo.prisma.flashCard.findUnique(
-      {
-        where: { id: id }
-      }
-    ));
+    result.push(
+      await this.repo.flashCard.findUnique({
+        where: { id: id },
+      })
+    );
     return result;
   }
 
   async findByName(name: string): Promise<FlashCard[]> {
-    const result = await this.repo.prisma.flashCard.findMany({
-
+    const result = await this.repo.flashCard.findMany({
       where: {
         name: {
-          contains: name
-        }
-      }
+          contains: name,
+        },
+      },
     });
     return result;
   }
 
   async findAll(): Promise<FlashCard[]> {
-    return await this.repo.prisma.flashCard.findMany();
+    return await this.repo.flashCard.findMany();
   }
 
   async create(data: FlashCard): Promise<FlashCard> {
     // const new_id = new ObjectId();
-    return await this.repo.prisma.flashCard.create({
-      data
-    }
-    );
+    return await this.repo.flashCard.create({
+      data,
+    });
   }
 
   async delete(id: any): Promise<FlashCard> {
-    return await this.repo.prisma.flashCard.delete({
+    return await this.repo.flashCard.delete({
       where: { id },
     });
   }
