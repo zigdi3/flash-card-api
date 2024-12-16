3e5a54564fb2005864c8ce1db3af6235cd239b8e
Diego Araujo <diegozigoto@gmail.com>
2024-12-16 19:01:10 -0300

    🚅 perfs: disable auto-spin

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppService } from './app.service';
 import { initializeMiddlewares } from './core/middlewares';
-import { TimeoutInterceptor } from './shared/time-out-interceptor';
-import { SpinUpService } from './modules/infra/spinUp';
 
 //pre initialiaze
 const core = new AppService();
 
-const spin = new SpinUpService();
+// const spin = new SpinUpService();
 /**
  * Start app
  */
@@ -25,7 +22,7 @@ async function bootstrap() {
   const port = 3131;
   await app.listen(port, () => {
     // logger.log(`Application is running on: ${await app.getUrl()}`);
-    console.log(`API running on port: ${port} `), spin.onModuleInit();
+    // console.log(`API running on port: ${port} `), spin.onModuleInit();
   });
 }
 //run app
